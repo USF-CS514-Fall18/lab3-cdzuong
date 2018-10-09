59cc25f44d7d8347fd1833874721df6b6dc5f3bc
Olga Karpenko <okarpenko@usfca.edu>
2018-10-08 18:03:46 -0700

    Adding starter code (mostly input files) for lab3, Movie Recommender.

---
+++ src/recommender/MovieRecommender.java
@@ -0,0 +1,20 @@
+package recommender;
+
+public class MovieRecommender {
+    // FILL IN CODE
+    // Add instance data, methods in this class
+    // and create other classes as needed
+
+    /**
+     * Takes four command line arguments:
+     *  args[0] path to the folder that contains movies and ratings files
+     *  args[1] path to the folder where  to save recommendations and antiRecommendations
+     *  args[2] id of the user for whom to compute recommendations (and anti-recommendations)
+     *  args[3] number of recommendations (and anti-recommendations) to write to a file.
+     * @param args argument to the program
+     */
+    public static void main(String[] args) {
+        // FILL IN CODE
+
+    }
+}
