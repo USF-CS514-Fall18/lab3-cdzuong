07f8dd95b302fc4271c1a403b63f98535aad92ad
caseydzuong <cdzuong@gmail.com>
2018-10-16 08:01:26 -0700

    Completed file upload and split method, need to revise and debug as necessary.

--- src/recommender/Ratings.java
+++ src/recommender/Ratings.java
@@ -5,21 +5,36 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Ratings {
-    private int userId;
-    private int movieId;
+
+    private String userId;
+    private String movieId;
     private double rating;
     private double timestamp;
 
     public Ratings(String filename) {
-       try {
-           File file = new File("input/newSet/ratings.csv");
-           Scanner input = new Scanner(file);
+        try {
+            File file = new File("input/newSet/ratings.csv");
+            Scanner input = new Scanner(file);
+            String splitLine[] = input.nextLine().split(", ");
+            for (int i = 0; i < splitLine.length; i++) {
+                if (i % 4 == 0) {
+                    userId = splitLine[i];
+                }
+                if (i % 4 == 1) {
+                    movieId = splitLine[i];
+                }
+                if (i % 4 == 2) {
+                    rating = Double.parseDouble(splitLine[i]);
+                }
+                if (i % 4 == 3) {
+                    timestamp = Double.parseDouble(splitLine[i]);
+                }
 
+            }
 
-
-       } catch (FileNotFoundException e) {
-           System.out.println("File not found.");
-       }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
 
 
     }
